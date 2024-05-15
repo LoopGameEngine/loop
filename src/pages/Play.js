d3ab1ab29df66ada89367d5c00c6b4c584b0f396
loopgameengine <loopgameengine@gmail.com>
2024-05-15 18:05:29 +0200

    15/05

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,11 +1,13 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useState } from 'react';
 import { useAppContext } from '../AppContext';
+import MadeWith from './sections/MadeWith';
 
 function Play() {
     const { gameID, token, API_KEY, DISCOVERY_DOCS } = useAppContext();
     const iframeRef = useRef(null);
+    const [isFullscreen, setIsFullscreen] = useState(false);
 
-    const handleOpenEngine = useCallback((fullscreen) => {
+    const handleOpenEngine = useCallback(() => {
         if (iframeRef.current) {
             const messageData = {
                 type: 'playGame',
@@ -15,24 +17,54 @@ function Play() {
         }
     }, [gameID, token, API_KEY, DISCOVERY_DOCS]);
 
+    const requestFullscreen = () => {
+        const element = document.documentElement;
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) { // Firefox
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) { // IE/Edge
+            element.msRequestFullscreen();
+        }
+        setIsFullscreen(true);
+    };
+
+    const startGame = (fullscreen) => {
+        if (fullscreen) {
+            requestFullscreen();
+        } else {
+            setIsFullscreen(true);
+        }
+    };
+
     return (
-        <div style={{ position: 'relative', width: '100vw', height: '100vh', overflow: "hidden" }}>
-            <iframe
-                title="Game Engine"
-                ref={iframeRef}
-                id="game-iframe"
-                onLoad={() => handleOpenEngine(false)}
-                src="/engine/"
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    border: 'none',
-                }}
-            ></iframe>
+        <div style={{ position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden', background: '#353535' }}>
+            {isFullscreen ? (
+                <iframe
+                    title="Game Engine"
+                    ref={iframeRef}
+                    id="game-iframe"
+                    onLoad={handleOpenEngine}
+                    src="/engine/"
+                    style={{
+                        width: '100vw',
+                        height: '100vh',
+                        border: 'none',
+                        position: 'absolute',
+                        top: 0,
+                        left: 0
+                    }}
+                ></iframe>
+            ) : (
+                <MadeWith
+                    onFullscreen={() => startGame(true)}
+                    onWindowed={() => startGame(false)}
+                />
+            )}
         </div>
     );
 }
 
 export default Play;
-
-
