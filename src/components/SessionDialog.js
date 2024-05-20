69519be8005a65f5cc819a94cf3f63cb2be1b5fc
loopgameengine <loopgameengine@gmail.com>
2024-05-20 16:49:30 +0200

    20/05

--- src/components/SessionDialog.js
+++ src/components/SessionDialog.js
@@ -1,20 +1,20 @@
-// SessionDialog.js
 import React from 'react';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from '@mui/material';
 
 const SessionDialog = ({ open, onLogin, onClose }) => (
   <Dialog open={open} onClose={onClose}>
-    <DialogTitle>Your session has expired</DialogTitle>
+    <DialogTitle>Your session is about to expire</DialogTitle>
     <DialogContent>
       <DialogContentText>
         Would you like to log in again?
       </DialogContentText>
     </DialogContent>
     <DialogActions>
-      <Button onClick={onLogin}>Login</Button>
-      <Button onClick={onClose}>Cancel</Button>
+      <Button onClick={onLogin} color="primary">Login</Button>
+      <Button onClick={onClose} color="secondary">Cancel</Button>
     </DialogActions>
   </Dialog>
 );
 
 export default SessionDialog;
+
